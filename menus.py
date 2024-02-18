f69f1662ae82dffbca30f39da4cd82ccf81fede1
Lenkly2 <143933790+Lenkly2@users.noreply.github.com>
2024-02-18 13:12:24 +0200

    Add files via upload

---
+++ menus.py
@@ -0,0 +1,35 @@
+import pygame
+import pygame_menu
+
+pygame.init()
+sw = 1366
+sh = 768
+window = pygame.display.set_mode((sw,sh))
+font = pygame_menu.font.FONT_8BIT
+mytheme = pygame_menu.Theme(widget_font=font,background_color=(3,45,123,0),
+                                title_background_color = (255,255,255))
+def start():
+    print("negro")
+def end():
+    pygame_menu,exit()
+
+def help():
+    
+    help_main = pygame_menu.Menu("Main menu",sw,sh,theme=mytheme)
+    def returnen():
+        main.enable()
+        help_main.disable()
+    help_main.add.button("Exit",end)
+    help_main.add.button("return",returnen)
+    main.disable()
+    help_main.mainloop(window)
+    
+
+main = pygame_menu.Menu("Main menu",sw,sh,theme=pygame_menu.themes.THEME_DARK.copy())
+main.add.button("Запустити гру",start)
+main.add.button("Зупинити гру",end)
+main.add.button("help",help)
+main.mainloop(window)
+
+
+
